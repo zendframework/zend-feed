6e478c96e14380cb20a6cbc14824b4940c872d77
Rob Loach <robloach@gmail.com>
2015-11-25 18:00:40 -0500

    Remove use of $GLOBALS['HTTP_RAW_POST_DATA']
    Fixes #10

--- src/PubSubHubbub/AbstractCallback.php
+++ src/PubSubHubbub/AbstractCallback.php
@@ -280,9 +280,6 @@ abstract class AbstractCallback implements CallbackInterface
     protected function _getRawBody()
     {
         $body = file_get_contents('php://input');
-        if (strlen(trim($body)) == 0 && isset($GLOBALS['HTTP_RAW_POST_DATA'])) {
-            $body = $GLOBALS['HTTP_RAW_POST_DATA'];
-        }
         if (strlen(trim($body)) > 0) {
             return $body;
         }
