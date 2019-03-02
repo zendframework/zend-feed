0c48e73c2f0f2d575f72b57e53583585b3302655
Frank Brückner <dev@froschdesignstudio.de>
2019-03-02 09:04:50 +0100

    Marks old and unused reader classes as deprecated

--- src/Reader/Collection.php
+++ src/Reader/Collection.php
@@ -11,6 +11,11 @@ namespace Zend\Feed\Reader;
 
 use ArrayObject;
 
+/**
+ * @deprecated This class is deprecated. Use the concret collection classes
+ *             \Zend\Feed\Reader\Collection\Author and \Zend\Feed\Reader\Collection\Category
+ *             or the generic class \Zend\Feed\Reader\Collection\Collection instead
+ */
 class Collection extends ArrayObject
 {
 }
